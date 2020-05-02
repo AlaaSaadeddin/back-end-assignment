f14cd248bd4b72ddbf656749ba5f943e3de604f2
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-05-03 01:40:32 +0300

    add connection.js file relates #2

---
+++ server/database/connection.js
@@ -0,0 +1,16 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+let dbUrl ;
+if(process.env.NODE_ENV === 'development'){
+dburl = process.env.DB_URL;
+} else{
+    throw new Error('No Database URL');
+}
+
+const options = {
+    connectionString : dbUrl,
+    ssl : true
+}
+
+module.exports = new Pool(options);
