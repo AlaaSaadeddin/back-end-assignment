198e0edfc4629e6cc9c4b59bd96af5cf60d154cb
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-05-04 22:55:14 +0300

    add sign up route relates #15

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cookie_parser = require('cookie-parser');
 
+const router = require('../server/routes/userRoutes');
+const serverError = require('../server/controllers/handleError/error');
+
 const app = express();
 
 app.disable('x-powered-by');
 app.use(express.urlencoded({extended : false}));
 app.use(cookie_parser);
 
+app.use('/api/' , router)
+app.use(serverError);
+
 module.exports = app;
+
