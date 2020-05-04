531998a722ecea45c2a8267b48dd7f55e9ba455d
AlaaSaadeddin <alaa.swaireh@gmail.com>
2020-05-04 22:25:59 +0300

    add a sign up query relates #4

--- server/database/queries/signUpUser.js
+++ server/database/queries/signUpUser.js
@@ -2,7 +2,7 @@ const connection = require('../connection');
 
 const addUSer = (data) => {
     const {username , email , password} = data
-   return connection.query('INSERT INTO user_info (username,email,password) VALUES ($1,$2,$3)',[username,email,password]);
+   return connection.query('INSERT INTO user_info (username,email,password) VALUES ($1,$2,$3)', [username,email,password]);
 }
 
 module.exports = addUSer;
